fd176f61c0aa1b3b41dcea7910a82f4f0bf4a1ac
TaylorYT29 <taylorduransibaja@gmail.com>
2024-09-01 19:23:36 -0700

    Models

---
+++ app/Models/Favorite_post_place.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite_post_place extends Model
+{
+    use HasFactory;
+}
