a4a007c0993ce499c1ef09c0ea9954f2079cb15c
AsuncionPD <asuncion.pena@ucr.ac.cr>
2024-09-04 22:04:51 -0600

    Modified users in migrations
    Se modifico en la migración de usuarios el tipo de dato

--- database/migrations/2024_08_31_071927_create_users.php
+++ database/migrations/2024_08_31_071927_create_users.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->unsignedBigInteger('user_type_id')->nullable();
-            $table->integer('location');
+            $table->string('location');
             $table->timestamps();
 
             // Indexes
