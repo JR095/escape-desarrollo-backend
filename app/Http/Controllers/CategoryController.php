35417e1fb37ecf464fae252f071c97d33f4374f2
tintin1401 <dana.cardenascr@gmail.com>
2024-10-06 18:47:13 -0600

    imgs

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -44,15 +44,43 @@ class CategoryController extends Controller
     public function getItems($id1, $id2, $id3, $id4)
     {
        
-        $items = Company::when($id1 != 0, function($query) use ($id1) {
+        $items = Company::select(
+            'companies.id',
+            'companies.name',
+            'companies.phone_number',
+            'companies.user_type_id',
+            'categories.name as category_id',
+            'sub_categories.name as sub_category_id',
+            'companies.email',
+            'companies.description',
+            'companies.image',
+            'cantons.name as canton_id',
+            'districts.name as district_id',
+            'companies.latitude',
+            'companies.longitude',
+            'companies.followers_count'
+        )
+        ->join('categories', 'companies.category_id', '=', 'categories.id')
+        ->join('sub_categories', 'companies.sub_categories_id', '=', 'sub_categories.id')
+        ->join('cantons', 'companies.canton_id', '=', 'cantons.id')
+        ->join('districts', 'companies.district_id', '=', 'districts.id')
+        ->when($id1 != 0, function($query) use ($id1) {
             return $query->where('companies.category_id', $id1);
-        })->when($id2 != 0, function($query) use ($id2) {
+        })
+        ->when($id2 != 0, function($query) use ($id2) {
             return $query->where('companies.sub_categories_id', $id2);
-        })->when($id3 != 0, function($query) use ($id3) {
+        })
+        ->when($id3 != 0, function($query) use ($id3) {
             return $query->where('companies.canton_id', $id3);
-        })->when($id4 != 0, function($query) use ($id4) {
+        })
+        ->when($id4 != 0, function($query) use ($id4) {
             return $query->where('companies.district_id', $id4);
-        })->get();
+        })
+        ->get();
+
+        foreach ($items as $activity) {
+            $activity->image = "http://localhost/escape-desarrollo-backend/public/imgs/".$activity->image;
+        }
         
          return response()->json($items,200, [], JSON_UNESCAPED_UNICODE);
     }
