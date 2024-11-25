12788286587585b145a59b631c30fc411d727c60
tintin1401 <dana.cardenascr@gmail.com>
2024-11-25 17:30:07 -0600

    delete canton

--- database/seeders/CantonSeeder.php
+++ database/seeders/CantonSeeder.php
@@ -15,14 +15,5 @@ class CantonSeeder extends Seeder
     {
         Canton::create(['name'=>'Puntarenas']);
         Canton::create(['name'=>'Esparza']);
-        Canton::create(['name'=>'Buenos Aires']);
-        Canton::create(['name'=>'Montes de Oro']);
-        Canton::create(['name'=>'Osa']);
-        Canton::create(['name'=>'Aguirre']);
-        Canton::create(['name'=>'Golfito']);
-        Canton::create(['name'=>'Coto Brus']);
-        Canton::create(['name'=>'Parrita']);
-        Canton::create(['name'=>'Corredores']);
-        Canton::create(['name'=>'Garabito']);
     }
 }
